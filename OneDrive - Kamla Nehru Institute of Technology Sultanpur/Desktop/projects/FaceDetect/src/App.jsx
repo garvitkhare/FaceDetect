d470efe5fbb5dd6c1301619d720003047c358140
garvitkhare <garvitkh03@gmail.com>
2024-09-03 01:08:22 +0530

    Configure React app for deployment to GitHub Pages

---
+++ OneDrive - Kamla Nehru Institute of Technology Sultanpur/Desktop/projects/FaceDetect/src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import Navbar from "./components/Navbar";
+import NewPost from "./components/NewPost";
+
+function App() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        setImage({
+          url: img.src,
+          width: img.width,
+          height: img.height,
+        });
+      };
+    };
+
+    file && getImage();
+  }, [file]);
+
+  return (
+    <div>
+      <Navbar />
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src="http://ts1.mm.bing.net/th?id=OIP.JdaDLGdTRYoZmqaiig7IpAHaHa&pid=15.1"
+              alt=""
+              className="avatar"
+            />
+            <div className="postForm">
+              <h2>Add your image to detect faces</h2>
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                
+                <button>Submit</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ OneDrive - Kamla Nehru Institute of Technology Sultanpur/Desktop/projects/FaceDetect/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+             <span id="head">FaceDetect</span>
+        </div>
+    )
+}
+
+export default Navbar
